139c0c4bcf4974cd5de168bc993b8d6befad6d3a
Alexander Sareyani <rievandesign@gmail.com>
2016-08-15 10:33:30 -0400

    changed all files to php

---
+++ site/businesssolutions/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<!-- Website template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Business Solutions</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
+	<!--[if lte IE 7]>
+		<link rel="stylesheet" href="css/ie.css" type="text/css" charset="utf-8" />	
+	<![endif] this is a tes-->
+</head>
+
+<body>
+	<div id="header">
+        
+		<a href="index.html" id="logo"><img src="images/logo4.jpg" width="100"/><img src="images/logo.jpg" alt="LOGO"/> <b> <font size="3" color="white" style="padding-top: -15px;"/>(813)-446-1019 </font></b></a>
+
+<?php require_once ('menu.php'); ?>
+
+	</div> <!-- /#header -->
+	<div id="adbox">
+		<div class="body">
+			<div class="images">
+				<img src="images/discussing2.jpg" alt="Img" class="preview" />
+				<img src="images/shake-hands.jpg" alt="Img" height="144" width="230px" />
+				<img src="images/professionals.jpg" alt="Img" height="135" width="230px" class="last" />
+			</div>
+			<div class="details">
+				<p>
+					<span>
+						Rievan Design focuses on your needs and builds systems to increase your bottom line. 
+					</span>
+					Web design is not simply about building a pretty website. Rievan Design builds the systems your company needs to operate effectively in theis ever changing digital landscape. Don't fall behind, stay upfront of technolodgy and call for your free consultation. <ins><ins></ins></ins>
+				</p>
+			</div>
+		</div>
+		<div class="footer" style="padding-top: 130px;">
+			<ul>
+				<li class="selected">
+					<a href="index.html"><img src="images/meeting2.jpg" alt="Img" /></a>
+					<p>
+						<a href="index.html">We take the time to understand your business.</a><br/>
+						There is no cookie cutter solution for a growong company. We design the solution to fit your situation.
+					</p>
+				</li>
+				<li>
+					<a href="index.html"><img src="images/flags2.jpg" alt="Img" /></a>
+					<p>
+						<a href="index.html">Business Operations</a><br/>
+						Primarley located in the United States Our team does higher memers outside. We specialize in 100% uptime. What this means is that we can work around the clock to make sure your business never stops. By hireing outsite the U.S. we can be sure that people are working ever minute of the day. 
+					</p>
+				</li>
+				<li>
+					<a href="index.html"><img src="images/boys.jpg" alt="Img" /></a>
+					<p>
+						<a href="index.html">Team Management</a><br/>
+						Based on the size of your project we fit the team to fit your needs. As we all know two heads are better than one. We pick the best suited members to approach your project. 
+					</p>
+				</li>
+			</ul>
+			<span class="bottom-shadow"></span>
+		</div>
+	</div> <!-- /#adbox -->
+	<div id="contents"style="padding-top: 200px;">
+		<div class="body">
+			<div id="sidebar">
+				<h3>Blog</h3>
+				<ul>
+					<li>
+						Quisque iaculis erat in velit fermentum pellentesque. 
+						<span>Sept. 21 | by Nullam <a href="blog.html">8</a></span>
+					</li>
+					<li>
+						Semper. Cras eu elit velit. Nullam vel eros turpis.
+						<span>Sept. 21 | by Nullam <a href="blog.html">8</a></span>
+					</li>
+					<li>
+						Aenean id erat elit, quis venenatis erat. 
+						Cras ornare orci vitae metus 
+						<span>Sept. 21 | by Nullam <a href="blog.html">8</a></span>
+					</li>
+					<li>
+						Vivamus euismod dictum purus ac semper. 
+						Etiam sed elit id ante commodo
+						<span>Sept. 21 | by Nullam <a href="blog.html">8</a></span>
+					</li>
+					<li>
+						Fusce ac quam nibh, at pharetra dolor. 
+						In quis odio arcu, vel laoreet eros. 
+						Nam vel sem dui. 
+						<span>Sept. 21 | by Nullam <a href="blog.html">8</a></span>
+					</li>
+				</ul>
+			</div>
+			<div id="main" style="height: 700px;">
+				<span>Rievan Design prides it's self on keeping up with all technoligies that improve the web viability of small to large companies. </span>
+				<ul>
+					<li>
+						<img src="images/globe.jpg" alt="Img" /><h3>Servers and Network</h3><p>Our partners are 100% the best. Price and reliability.</p>
+					</li>
+					<li>
+						<img src="images/tools.jpg" alt="Img" /><h3>In the Code</h3><p>Our programmers are required to have at least 10 years expierence.</p>
+					</li>
+					<li>
+						<img src="images/check.jpg" alt="Img" /><h3>Management</h3>
+						<p>Our project managers are always there to assist. We don't stop untill your are 100% satisifed.</p>
+					</li>
+					<li>
+						<img src="images/graph.jpg" alt="Img" /><h3>Our Goal</h3>
+						<p>At the end of the day nothing matters unless you are successfull. We have 10 years of affiliate marketing expierence as well as S.E.O. and direct response marketing expierence.</p>
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</div> <!-- /#contents -->
+	<div id="footer">
+		<ul class="contacts">
+			<h3>Contact Us</h3>
+			<li><span>Email</span><p>: company@email.com</p></li>
+			<li><span>Address</span><p>: 189 Lorem Ipsum Pellentesque, Mauris Etiam ut velit odio Proin id nisi enim 0000</p></li>
+			<li><span>Phone</span><p>: 117-683-9187-000</p></li>
+		</ul>
+		<ul id="connect">
+			<h3>Get Updated</h3>
+			<li><a href="blog.html">Blog</a></li>
+			<li><a href="http://facebook.com/freewebsitetemplates" target="_blank">Facebook</a></li>
+			<li><a href="http://twitter.com/fwtemplates" target="_blank">Twitter</a></li>
+		</ul>
+		<div id="newsletter">
+			<p><b>Sign-up for Newsletter</b>
+				In sollicitudin vulputate metus, sed commodo diam elementum nec. Sed et risus sed magna convallis adipiscing.
+			</p>
+			<form action="" method="">
+				<input type="text" value="Name" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
+				<input type="text" value="Enter Email Address" class="txtfield" onblur="javascript:if(this.value==''){this.value=this.defaultValue;}" onfocus="javascript:if(this.value==this.defaultValue){this.value='';}" />
+				<input type="submit" value="" class="button" />
+			</form>
+		</div>
+		<span class="footnote">&copy; Copyright &copy; 2011. All rights reserved</span>
+	</div> <!-- /#footer -->
+</body>
+</html>
---
+++ site/businesssolutions/menu.php
@@ -0,0 +1,11 @@
+		<div id="navigation">
+			<ul>
+				<li class="first selected"><a href="index.html">Home</a></li>
+				<li><a href="about.html">About us</a></li>
+				<li><a href="services.html">Services</a></li>
+				<li><a href="solutions.html">Solutions</a></li>
+				<li><a href="support.html">Support</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
